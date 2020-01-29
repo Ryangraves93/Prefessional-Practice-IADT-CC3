b519263aefd49c896b4d6d055bbd3c22ae7505be
Ryangraves93 <ryangraves1993@gmail.com>
2020-01-29 15:28:52 +0000

    Began working on player animation. Implemented aniamtor controller and set two states - Idle & Running

---
+++ Level Prototype - Ryan/Assets/Scripts/AnimatorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    public Transform player, destination;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       StartCoroutine(RunAnimation());
+    }
+
+    public IEnumerator RunAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("IsWalking", true);
+            yield return new WaitForSeconds(10f);
+        }
+        else if (player.position == destination.position)
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+}
